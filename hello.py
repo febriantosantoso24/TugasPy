8ef8a4048eb787e6da92f3c63c00551a1950f81d
Febrianto Santoso R <santosofebrian24@gmail.com>
2026-09-07 11:32:16 +0700

    kedua

--- hello.py
+++ hello.py
@@ -2,5 +2,7 @@ import sys
 print("Praktek Pemrograman Terapan - Pertemuan 1")
 print("Nama :", "___Febrianto Santoso R___")
 print("NIM :", "___33132501009___")
+print("Jurusan :", "___Teknologi Elektro___")
 print("Prodi :", "___Teknologi Rekayasa Otomasi___")
+print("JK :", "___lakik___")
 print("Python:", sys.version)
